7be0c1e097df129460b40702de91609c333c3e01
Aditya Mehra <aix.m@outlook.com>
2018-09-12 16:36:04 +0930

    Update skiller.sh

--- skiller.sh
+++ skiller.sh
@@ -5,11 +5,14 @@ then
   cd ~/mycroft-core
   source .venv/bin/activate
   msm install $1
+  oname=$(echo $1 | awk -F"/" '{print $5}')
+  cd /opt/mycroft/skills/$oname.*/ui/ && mkdir -p /opt/mycroft/skills/ui/skills/$oname/ && mv *.qml /opt/mycroft/skills/ui/skills/$oname
 else
   cd /opt/mycroft/skills/ && rm -rf $2
   cd ~/mycroft-core
   source .venv/bin/activate
   msm install $1
-  oname=$(echo $1 | awk -F"/" '{print $5}') 
+  oname=$(echo $1 | awk -F"/" '{print $5}')
   cd /opt/mycroft/skills/ && mv $oname.* $2
+  cd /opt/mycroft/skills/$oname.*/ui/ && mkdir -p /opt/mycroft/skills/ui/skills/$oname/ && mv *.qml /opt/mycroft/skills/ui/skills/$oname
 fi
