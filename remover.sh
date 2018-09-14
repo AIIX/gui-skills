1faaa71c2e251c4a1d0d9c18606e0428b3104595
Aditya Mehra <aix.m@outlook.com>
2018-09-14 19:43:52 +0930

    Update remover.sh

--- remover.sh
+++ remover.sh
@@ -2,4 +2,5 @@
 echo $1
 echo "Removing Skill $1"
 cd /opt/mycroft/skills && rm -rf $1
-cd /opt/mycroft/skills/ui/skills && rm -rf $2 
+oname=$(echo $1 | awk -F"." '{print $1}')
+cd /opt/mycroft/skills/ui/skills && rm -rf oname 
