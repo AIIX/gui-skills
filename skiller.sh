bc697aca402a0a44e8e46d29f91d24389bf3475c
Aditya Mehra <aix.m@outlook.com>
2018-09-18 00:11:30 +0930

    Update skiller.sh

--- skiller.sh
+++ skiller.sh
@@ -7,7 +7,7 @@ then
   source .venv/bin/activate
   msm install $1
   oname=$(echo $1 | awk -F"/" '{print $5}')
-  if cd /opt/mycroft/skills/$oname.*/ui/ 
+  if (cd /opt/mycroft/skills/$oname.*/ui/) > /dev/null 2>&1 
   then
      echo "INFO - Installing GUI Skill: $oname"
      cd /opt/mycroft/skills/$oname.*/ui/ && mkdir -p /opt/mycroft/skills/ui/skills/$oname/ && mv * /opt/mycroft/skills/ui/skills/$oname
@@ -25,7 +25,7 @@ else
   oname=$(echo $1 | awk -F"/" '{print $5}')
   gname=$(echo $2 | awk -F"." '{print $1}')
   cd /opt/mycroft/skills/ && mv $oname.* $2
-  if cd /opt/mycroft/skills/$2/ui/
+  if (cd /opt/mycroft/skills/$2/ui/) > /dev/null 2>&1
   then
     echo "INFO - Installing GUI Skill: $gname"
     cd /opt/mycroft/skills/$2.*/ui/ && mkdir -p /opt/mycroft/skills/ui/skills/$2/ && mv * /opt/mycroft/skills/ui/skills/$gname
