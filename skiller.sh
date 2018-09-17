cec557add855309369f31554bed842493fead9ce
Aditya Mehra <aix.m@outlook.com>
2018-09-17 20:10:53 +0930

    Update skiller.sh

--- skiller.sh
+++ skiller.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 cd /opt/mycroft/skills
 if [ -z "$2" ]
+##Stage1 New Skill
 then
   cd ~/mycroft-core
   source .venv/bin/activate
   msm install $1
   oname=$(echo $1 | awk -F"/" '{print $5}')
-  cd /opt/mycroft/skills/$oname.*/ui/ && mkdir -p /opt/mycroft/skills/ui/skills/$oname/ && mv * /opt/mycroft/skills/ui/skills/$oname
+  if cd /opt/mycroft/skills/$oname.*/ui/ 
+  then
+     echo "INFO - Installing GUI Skill: $oname"
+     cd /opt/mycroft/skills/$oname.*/ui/ && mkdir -p /opt/mycroft/skills/ui/skills/$oname/ && mv * /opt/mycroft/skills/ui/skills/$oname
+     echo "INFO - Mycroft GUI Skill: $oname Installed"
+  else
+     echo "INFO - Skill: $oname doesn't have an UI folder skiping UI installation"
+     echo "INFO - Mycroft Skill: $oname Installed"
+  fi
+##Stage2 Replace Orginal Skill
 else
   cd /opt/mycroft/skills/ && rm -rf $2
   cd ~/mycroft-core
   source .venv/bin/activate
   msm install $1
   oname=$(echo $1 | awk -F"/" '{print $5}')
+  gname=$(echo $2 | awk -F"." '{print $1}')
   cd /opt/mycroft/skills/ && mv $oname.* $2
-  cd /opt/mycroft/skills/$oname.*/ui/ && mkdir -p /opt/mycroft/skills/ui/skills/$oname/ && mv * /opt/mycroft/skills/ui/skills/$oname
+  if cd /opt/mycroft/skills/$2/ui/
+  then
+    echo "INFO - Installing GUI Skill: $gname"
+    cd /opt/mycroft/skills/$2.*/ui/ && mkdir -p /opt/mycroft/skills/ui/skills/$2/ && mv * /opt/mycroft/skills/ui/skills/$gname
+    echo "INFO - Mycroft GUI Skill: $gname Installed"
+  else
+     echo "INFO - Skill: $gname doesn't have an UI folder skiping UI installation"
+     echo "INFO - Mycroft Skill: $gname Installed"
+  fi
 fi
