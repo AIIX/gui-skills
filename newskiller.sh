df7cc01595342ed4685105c320198a3eb17db80a
Aditya Mehra <aix.m@outlook.com>
2018-12-13 16:14:26 +0530

    Update newskiller.sh

--- newskiller.sh
+++ newskiller.sh
@@ -5,11 +5,9 @@ if [ -z "$2" ]
 then
   cd ~/mycroft-core
   source .venv/bin/activate
-  msm install $1
   oname=$(echo $1 | awk -F"/" '{print $5}')
   echo "INFO - Installing GUI Skill: $oname"
-  cd /opt/mycroft/skills/$oname.*/ && git checkout $3 && git reset --hard origin/$3
-  echo "INFO - Mycroft GUI Skill: $oname Installed"
+  msm install $1
 ##Stage2 Replace Orginal Skill
 else
   cd /opt/mycroft/skills/ && rm -rf $2
@@ -19,13 +17,4 @@ else
   oname=$(echo $1 | awk -F"/" '{print $5}')
   gname=$(echo $2 | awk -F"." '{print $1}')
   cd /opt/mycroft/skills/ && mv $oname.* $2
-  if (cd /opt/mycroft/skills/$2/ui/) > /dev/null 2>&1
-  then
-    echo "INFO - Installing GUI Skill: $gname"
-    cd /opt/mycroft/skills/$gname.*/ && git checkout $3 && git reset --hard origin/$3
-    echo "INFO - Mycroft GUI Skill: $gname Installed"
-  else
-     echo "INFO - Skill: $gname doesn't have an UI folder skiping UI installation"
-     echo "INFO - Mycroft Skill: $gname Installed"
-  fi
 fi
