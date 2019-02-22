323811c75914e840a08773caf99d487bb9b92a0e
Aditya Mehra <aix.m@outlook.com>
2019-02-22 18:04:26 +0530

    Update brancher.sh

--- brancher.sh
+++ brancher.sh
@@ -6,3 +6,7 @@ echo "INFO - Installing GUI Skill Branch"
 git fetch
 git checkout $2 && git reset --hard origin/$2
 echo "INFO - Mycroft GUI Branch Installed"
+echo "INFO - Installing Requirements Workaround"
+source /opt/mycroft/.venv/bin/activate
+pip install -r requirements.txt
+echo "INFO - Installed Requirements"
