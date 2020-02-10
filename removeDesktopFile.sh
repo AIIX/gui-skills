8e0021b9f3f3d642de0ebd41e27a98a85427bbd9
Aditya Mehra <aix.m@outlook.com>
2020-02-11 01:26:56 +1030

    Update removeDesktopFile.sh

--- removeDesktopFile.sh
+++ removeDesktopFile.sh
@@ -3,7 +3,7 @@ echo "INFO - Cleaning Desktop File For $1"
 if cd /opt/mycroft/skills/$1
 then
 cd /opt/mycroft/skills/$1/res/desktop/
-desktopFileName = "*.desktop"
+desktopFileName="*.desktop"
 echo "Info - Removing Skill Desktop File $desktopFileName"
 rm ~/.local/share/applications/$desktopFileName
 fi
