a728b1ed58525005c8dc20c14a1d19fbd34e2f3e
Aditya Mehra <Aix.m@outlook.com>
2020-02-11 02:07:27 +1030

    install icon

--- desktopFileInstaller.sh
+++ desktopFileInstaller.sh
@@ -3,9 +3,18 @@ if [ ! -d "~/.local/share/applications" ]; then
     cd ~/.local/share/
     mkdir applications
 fi
+if [ ! -d "~/.local/share/icons" ]; then
+    cd ~/.local/share/
+    mkdir icons
+fi
 cd $1
 echo "INFO - Skill folder $1"
 echo "INFO - Installing Desktop Files"
+if [ -d "res/icon" ]; then
+    cd res/icon
+    cp -r * ~/.local/share/icons
+    cd $1
+fi
 cd res/desktop
 cp -r * ~/.local/share/applications
 echo "INFO - Desktop File Installed"
